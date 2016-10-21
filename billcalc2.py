0edd6801e7bccfc8a47885e05c033b2e9622cd6f
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-20 21:17:17 -0700

    Bill calculator

---
+++ billcalc2.py
@@ -0,0 +1,30 @@
+def tip_calc(bill, tip_per):
+    tip_amt = bill * float(tip_per)/100
+    return tip_amt
+
+def total_bill(bill, tip_per):
+    tip_amt = bill * float(tip_per)/100
+    bill_amt = tip_amt + bill
+    return bill_amt
+
+def splitsies(bill, tip_per, party):
+    tip_amt = bill * float(tip_per)/100
+    bill_amt = tip_amt + bill
+    split_amt = bill_amt / party
+    return split_amt
+
+def main():
+    user_choice = int(raw_input("Pick a number: 1 - calculate tip, 2 - split the bill"))
+    if user_choice == 1:
+        bill = float(raw_input("What's the bill?"))
+        tip_per = float(raw_input("What tip percentage sounds good?"))
+        print "Your tip is ", tip_calc(bill, tip_per)
+        print "Your total bill is ", total_bill(bill, tip_per)
+    else:
+        bill = float(raw_input("What's the bill?"))
+        tip_per = float(raw_input("What tip percentage sounds good?"))
+        party = int(raw_input("How many people are in your party? (Enter a number)"))
+        print "Each person pays ", splitsies(bill, tip_per, party)
+
+if __name__ == '__main__':
+    main() 
